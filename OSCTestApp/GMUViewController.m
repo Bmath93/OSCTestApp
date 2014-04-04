3e7c09fb153a1cde24dbfd26e0cbc178fc2ab2b6
Bmath93 <Bmath93@gmail.com>
2014-04-04 16:02:53 -0400

    framework for sending OSC messages within app. no library installed yet.

--- OSCTestApp/GMUViewController.m
+++ OSCTestApp/GMUViewController.m
@@ -10,6 +10,14 @@
 
 @interface GMUViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *messageDisplayLabel;
+
+- (IBAction)sendMessage1:(id)sender;
+
+- (IBAction)sendMessage2:(id)sender;
+
+- (void)changeMessageDisplayLabelTo: (int)recievedInt;
+
 @end
 
 @implementation GMUViewController
@@ -26,4 +34,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sendMessage1:(id)sender {
+    [self changeMessageDisplayLabelTo:1];
+}
+
+- (IBAction)sendMessage2:(id)sender {
+    [self changeMessageDisplayLabelTo:2];
+}
+
+- (void)changeMessageDisplayLabelTo:(int)recievedInt{
+    NSString *newTextString = [NSString stringWithFormat:@"%i",recievedInt];
+    [self.messageDisplayLabel setText:newTextString];
+}
 @end
